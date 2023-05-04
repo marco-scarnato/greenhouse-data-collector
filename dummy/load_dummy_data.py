090d1cf7c50bc37ffc297d499da0cc5ffb4b2911
marcooamato <amatomarco111@gmail.com>
2023-05-04 11:22:07 +0200

    import influx from dummy fixed

--- dummy/load_dummy_data.py
+++ dummy/load_dummy_data.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 from influxdb_client import Bucket
 
-from influx.influx_controller import InfluxController
+from src.influx.influx_controller import InfluxController
 from dummy_measurements import POT_MEASUREMENTS, GREENHOUSE_MEASUREMENTS, SHELF_MEASUREMENTS, PUMP_MEASUREMENTS, \
     PLANT_MEASUREMENTS
 

df05fba2544073de39bce57f0c436b533a9e3da5
marcooamato <amatomarco111@gmail.com>
2023-05-05 12:36:20 +0200

    fixed import in main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import threading
 
-from assets.greenhouse_asset import GreenhouseAsset
-from assets.pot_asset import PotAsset
-from assets.shelf_asset import ShelfAsset
+from src.assets.greenhouse_asset import GreenhouseAsset
+from src.assets.pot_asset import PotAsset
+from src.assets.shelf_asset import ShelfAsset
 from src.sensors.humidity import Humidity
-from sensors.light_level import LightLevel
+from src.sensors.light_level import LightLevel
 from src.sensors.mcp3008 import MCP3008
 from src.sensors.moisture import Moisture
 from src.sensors.temperature import Temperature
@@ -17,7 +17,7 @@ def main():
     thread_shelf = threading.Thread(target=shelf_measurement.read_sensor_data())
     thread_shelf.start()
 
-    pot_measurement = PotAsset(1, 'right', 'left', Moisture(mcp3008, 1))
+    pot_measurement = PotAsset(1, 'right', 'left', "1", Moisture(mcp3008, 1))
     thread_pot = threading.Thread(target=pot_measurement.read_sensor_data())
     thread_pot.start()
 

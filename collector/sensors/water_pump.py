de79dbc32b8acbf40481148138aa1fb5994dbc16
Eduard Occhipinti <occhipinti.eduard@icloud.com>
2023-05-07 00:16:51 +0200

    first implementation, need to test on pysical hardware, problematic in virtual environments

---
+++ collector/sensors/water_pump.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+
+class WaterPump:
+    def __init__(self, pin):
+        self.pin = pin
+        GPIO.setup(self.pin, GPIO.OUT)
+        GPIO.output(self.pin, GPIO.LOW)
+
+    def on(self):
+        GPIO.output(self.pin, GPIO.HIGH)
+
+    def off(self):
+        GPIO.output(self.pin, GPIO.LOW)

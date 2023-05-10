e44b44a0926d90c53d29f58bc97e9a9a1d4628a5
marcooamato <amatomarco111@gmail.com>
2023-05-10 18:21:29 +0200

    config test file created

---
+++ collector/tests/test_config_eval.py
@@ -0,0 +1,26 @@
+import ast
+import json
+import threading
+import unittest
+from configparser import ConfigParser
+from typing import List
+
+from assets.pot_asset import PotAsset
+from sensors import mcp3008
+
+
+class MyTestCase(unittest.TestCase):
+    def test_eval_pot(self):
+        conf = ConfigParser()
+        # get conf from test file
+        conf.read("config_test.ini")
+        pots: List = json.loads(conf["ASSETS"]["pots"])
+        for pot_dict in pots:
+            pot = PotAsset(pot_dict, mcp3008)
+            print(pot.to_point().to_line_protocol())
+            # thread_pot = threading.Thread(target=pot.read_sensor_data)
+            # thread_pot.start()
+
+
+if __name__ == '__main__':
+    unittest.main()

001eb2247442bb52ee17e362e02e6929ee505743
Gianluca Bàrmina <70454516+N-essuno@users.noreply.github.com>
2023-05-19 14:56:52 +0200

    Fixed config.ini read

--- collector/config.py
+++ collector/config.py
@@ -2,7 +2,7 @@
 import os
 
 # Path to the configuration file, it should be in the same directory as this file
-CONFIG_PATH = os.path.join(os.path.dirname(__file__), "config.ini.example")
+CONFIG_PATH = os.path.join(os.path.dirname(__file__), "config.ini")
 
 # check if the path is to a valid file
 if not os.path.isfile(CONFIG_PATH):
